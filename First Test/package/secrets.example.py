2a0a8221154f9c770fcfdbff0692992b5d6fadeb
Joe J Collins <joejcollins@users.noreply.github.com>
2020-02-24 21:31:11 +0000

    Typos

--- First Test/package/secrets.example.py
+++ First Test/package/secrets.example.py
@@ -1,4 +1,4 @@
-''' Secrets not commited to version control '''
+''' Secrets should not committed to version control '''
 class Secrets(object):# pylint: disable=too-few-public-methods
     ''' Secrets '''
     COMPUTER_VISION_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
